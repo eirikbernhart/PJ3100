c5af7a3ae9f46ba3358405b05d95dd35c8808303
ADRIAN\Adrian <adrian_nintendo@hotmail.com>
2016-10-26 14:40:23 +0200

    Lagde Ny føring side

--- budsjettApp/src/app/app.component.ts
+++ budsjettApp/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import { NyFoering } from '../pages/ny-foering/ny-foering';
 
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage = NyFoering;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
--- budsjettApp/src/app/app.module.ts
+++ budsjettApp/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NyFoering } from '../pages/ny-foering/ny-foering';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    NyFoering
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    NyFoering
   ],
   providers: []
 })
