f719901de84cea23dfbd5cbaf9f5a0c492d59a66
huvkar15 <huvkar15@student.westerdals.no>
2016-10-26 14:14:51 +0200

    no message

---
+++ budsjettApp/src/pages/sparing/sparing.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the Sparing page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-sparing',
+  templateUrl: 'sparing.html'
+})
+export class Sparing {
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello Sparing Page');
+  }
+
+}
