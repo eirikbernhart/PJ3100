0e5f6a5ab6462982b4f1a1ef1b3398f57f0420c0
Kevin Frostad <kevin@Kevins-MacBook-Pro.local>
2016-10-28 12:50:37 +0200

    merget forside branch i master

--- budsjettApp/src/app/app.module.ts
+++ budsjettApp/src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Forside } from '../pages/forside/forside';
-import { HomePage } from '../pages/home/home';
 import { Tabs } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
     Forside,
-    HomePage,
     Tabs,
   ],
   imports: [
@@ -19,7 +17,6 @@ import { Tabs } from '../pages/tabs/tabs';
   entryComponents: [
     MyApp,
     Forside,
-    HomePage,
     Tabs
   ],
   providers: []
