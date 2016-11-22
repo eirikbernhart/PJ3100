ebbb2fe9902364c889014214e5129e112c380004
huvkar15 <huvkar15@student.westerdals.no>
2016-11-22 13:40:40 +0100

    login knapp. forsøk på push til en annen side

--- budsjettApp/src/app/app.component.ts
+++ budsjettApp/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 import { Tabs } from '../pages/tabs/tabs';
+import { Login } from '../pages/login/login';
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`,
 })
 export class MyApp {
 
-  public rootPage = Tabs;
+  public rootPage = Login;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
